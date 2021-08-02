e29dd8fa7822a6f6edf21749b89c977e4678789d
dkn1ght23 <mujammal.salman@gmail.com>
2021-08-02 15:54:57 +0600

    Sever file

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+
+const Uploader = require('./upload');
+//const MailSender = require('./mailsender');
+const eventHandlers = require('./eventhandlers');
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+
+
+const port = 3000;
+const hostName = 'localhost';
+
+let mediaDirectory = __dirname + '/media/';
+let fileUploader = new Uploader(mediaDirectory);
+
+
+
+let requestListener = (req, res) => {
+
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+    })
+
+    if(req.url === '/file-upload' && req.method === 'POST'){
+        fileUploader.upload(req, res);
+    }
+
+    else if(req.url === '/sendmail' && req.method === 'GET'){
+        //let sender = new MailSender();
+
+        let mailDetails = {
+            subject: 'Account registration',
+            body: 'Welcome onboard, Your code is 4a7s93',
+            to: 'tanzilebad@gmail.com'
+        }
+        //sender.send(req, res, mailDetails);
+        eventEmitter.emit('sendmail', mailDetails);
+        res.write('sending mail');
+        res.end();
+    }
+
+    else{
+        res.write('testing localhost');
+        res.end();
+    }
+
+}
+
+let onServerRun = (res) => {
+    eventHandlers(eventEmitter);
+    console.log(`server is running on local host ${3000}`);
+}
+
+http.createServer(requestListener).listen(port, hostName, onServerRun );
