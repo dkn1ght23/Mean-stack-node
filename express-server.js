d46448dcf5db0406283dc2fc51249a8ac60330e4
dkn1ght23 <mujammal.salman@gmail.com>
2021-08-04 20:53:57 +0600

    MiddleWare and Express server

--- express-server.js
+++ express-server.js
@@ -3,21 +3,25 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+//Middleware
+app.use('/welcome', (req,res, next) => {
+    res.send('bad request');
+})
+//Middleware
+//Every routes must go through middleware and if middleware wants to stop, it can do it.
+//Here, welcome is the key word, that is why server will show "bad request".
+
 app.get('/', (req,res) => {
-      res.send('Welcome to expressJS');
-})
-
-app.get('/welcome', (req,res) => {
-    res.send('Welcome to expressJS 2');
-})
-
-app.get('/sendmail', (req,res) => {
     let mailDetails = {
         subject: 'Account registration',
         body: 'Welcome onboard, Your code is 4a7s93',
         to: 'tanzilebad@gmail.com'
     }
     res.json(mailDetails);
+})
+
+app.get('/welcome', (req,res) => {
+    res.send('Hello mate!');
 })
 
 
